f5746b44a730668c34f6f5da187f1019623ed2e4
Ali Abubaker <aliabdulsameea69@gmail.com>
2026-07-19 14:58:51 +0300

    Fix WebOptimizer namespace and use KnownIPNetworks

--- PersonalInformation/Program.cs
+++ PersonalInformation/Program.cs
@@ -1,11 +1,30 @@
 using Microsoft.AspNetCore.HttpOverrides;
 using PersonalInformation.Components;
 using PersonalInformation.Services;
+using WebOptimizer;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// Add WebOptimizer for bundling and minification
+builder.Services.AddWebOptimizer(pipeline =>
+{
+    // Bundle all CSS files into one
+    pipeline.AddCssBundle("/css/bundle.css",
+        "wwwroot/css/tokens.css",
+        "wwwroot/css/base.css",
+        "wwwroot/css/animations.css",
+        "wwwroot/css/components.css",
+        "wwwroot/css/theme.css");
+
+    // Bundle all JS files into one
+    pipeline.AddJavaScriptBundle("/js/bundle.js",
+        "wwwroot/js/site.js",
+        "wwwroot/js/home.js");
+});
+
 builder.Services.AddScoped<ThemeService>();
 builder.Services.AddSingleton<BlogService>();
 builder.Configuration.AddUserSecrets<Program>();
@@ -17,12 +36,16 @@ builder.Services.AddHttpClient("Resend", client =>
 
 var app = builder.Build();
 
+// Use WebOptimizer middleware
+app.UseWebOptimizer();
+
 var forwardedHeaderOptions = new ForwardedHeadersOptions
 {
     ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
     ForwardLimit = null
 };
-forwardedHeaderOptions.KnownNetworks.Clear();
+// Fixed: Use KnownIPNetworks instead of KnownNetworks (obsolete)
+forwardedHeaderOptions.KnownIPNetworks.Clear();
 forwardedHeaderOptions.KnownProxies.Clear();
 app.UseForwardedHeaders(forwardedHeaderOptions);
 
